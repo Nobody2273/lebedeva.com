e31981340f4fcc219bb0a72993e3fba60170d083
Nobody <rus-44.meja@yandex.ru>
2025-04-22 21:45:04 +0300

    Добавлены страницы merch и photo

---
+++ pages/merch.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
+    <?php include '../includes/header.php'; ?>
+    <main>
+    <div class="three" align="center">
+            <p class="head"><b>Мерч</b></p>
+            <div class="mer">
+                <div>
+                    <img class="mer" src="/assets/images/mer1.jpg">
+                    <p class="mer_main"><b>Футболка 1</b></p>
+                    <p class="mer" style="margin-top: 30px">Футболка с принтом: "Котенок Ра"
+                        <br>Бежевая
+                    </p>
+                    <a class="mer" href="#">Купить &gt</a>
+                </div>
+                <div>
+                    <img class="mer" src="/assets/images/mer2.jpg">
+                    <p class="mer_main"><b>Футболка 2</b></p>
+                    <p class="mer">Футболка с принтом: 
+                        <br>"Корми демонов по расписанию"
+                        <br>Черно-белая
+                    </p>
+                    <a class="mer" href="#">Купить &gt</a>
+                </div>
+                <div>
+                    <img class="mer" src="/assets/images/mer3.jpg">
+                    <p class="mer_main"><b>Футболка 3</b></p>
+                    <p class="mer" style="margin-top: 30px">Футболка с принтом: "Котенок Ра"
+                        <br>Розовая
+                    </p>
+                    <a class="mer" href="#">Купить &gt</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php include '../includes/footer.php'; ?>
+</body>
+</html>
+
