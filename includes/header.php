4599851b334cf08a9962127a7eaacd439fabbeb4
Nobody <rus-44.meja@yandex.ru>
2025-04-22 11:57:34 +0300

    Добавлены header и footer

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/index.php">Главная</a></li>
+            <li><a href="/pages/merch.php">Мерч</a></li>
+            <li><a href="/pages/photo.php">Фотографии</a></li>
+        </ul>
+    </nav>
+</header>
