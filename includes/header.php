843c13db740e7b154c1f5333d55342882bb7a60f
Nobody <rus-44.meja@yandex.ru>
2025-04-22 22:00:52 +0300

    Реализовано динамическое меню

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,21 @@
+<?php
+$menuItems = [
+    'Главная' => '/index.php',
+    'Мерч' => '/pages/merch.php',
+    'Фотографии' => '/pages/photo.php'
+];
+?>
 <header>
+<div class="div_logo"><img class="logo" src="/assets/images/logo.png"></a></div>
     <nav>
-        <ul>
-            <li><a href="/index.php">Главная</a></li>
-            <li><a href="/pages/merch.php">Мерч</a></li>
-            <li><a href="/pages/photo.php">Фотографии</a></li>
+    <ul class="menu-list">
+            <?php foreach ($menuItems as $title => $url): ?>
+                <li>
+                    <a href="<?= $url ?>" <?= $_SERVER['REQUEST_URI'] == $url ? 'class="active"' : '' ?>>
+                        <?= $title ?>
+                    </a>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </nav>
 </header>
